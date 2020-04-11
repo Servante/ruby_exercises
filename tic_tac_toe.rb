723fb125baaf6f8944f9f925e631b42e88ca2fee
Wesley <matthew282@msn.com>
2020-04-10 17:32:34 -0700

    added game_turn and check_win methods

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -26,20 +26,22 @@ game.start
 
 game.turn
 
-1. check win - in progress
-2. checks turn count, assigns player.marker depending on if count is divisible by 2
-3. calls get player move
+1. check win - x
+2. checks turn count, assigns player.marker depending on if count is divisible by 2 - x
+3. calls get player move - x
 
 get_player_move
 
-1. create board
+1. create board - x
 2. ask player for move 
 3. check to see if valid move, assign if so
 
-game_win
+game_finish
 
 1. prints board
 2. prints congrats
+3. ask if wants to play again
+
 
 
 
@@ -48,13 +50,16 @@ game_win
 todo:
 
 build win parameters - x
-build check_win - in progress
-build game_turn method
-build get_player_move
+build check_win - x
+build game_turn method - x
+build get_player_move - in progress
+build game_finish -
+
+
 
 123
 456
-789
+789 
 
 
 
@@ -62,13 +67,14 @@ build get_player_move
 
 
 class Game 
-	attr_accessor :board
+	attr_accessor :board, :player1, :player2
 	win = [[1,2,3], [4,5,6], [7,8,9], [1,4,7],[1,5,9], [2,5,8],[3,6,9],[3,5,7]]
-	def initialize(player, player_marker)
-		@player = player
-		@player_marker = player_marker
+	def initialize(player1, player2)
+		@player1 = player1
+		@player2 = player2
 		@board = [" ", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 		@turn_count = 0
+
 	end
 
 	def create_board
@@ -79,22 +85,13 @@ class Game
 		}
 	end
 
-	def check_win(player)
-		count = 0
-		win.each {|x|
-			x.each {|x|
-				if player.moves_history.include?(x)
-					count += 1
-					if count >= 3
-						game.win
-
-			}
-		}
-
-
+	def game_turn
+		check_win
+		player = turn_count % 2 == 1 ? @player1 : @player2
+		get_move(player)
 	end
 
-	def game_start
+	def self.start
 		puts "Welcome to Tic Tac Toe"
 		print "Player 1(X) please enter your name: "
 		p1 = gets.chomp
@@ -103,27 +100,38 @@ class Game
 		playerone = Player.new(p1, "X")
 		playertwo = Player.new(p2, "O")
 		game = Game.new(playerone, playertwo)
-		game_turn
-
+		# game_turn
 	end
 
-	def game_turn
-
+	def check_win(player, player_moves)   #refactor
+		win.each do |x|
+			count = 0
+			x.each do |x|
+				count += 1 if player_moves.include?(x)
+				if count == 3
+					puts "Congrats #{player}, you win!"
+					game_finish
+				end
+			end
+		end
 	end
+
+	
+
+	def get_move(player)
+		create_board
+	end
+
 
 
 end
 
-
-
-
-
 class Player
-	attr_accessor :name, :token
+	attr_accessor :name, :token, :moves
 	def initialize(name, token)
 		@name = name
 		@token = token
-		@move_history = move_history
+		@moves = []
 	end
 end
 
