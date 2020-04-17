3c7669be7800041a843ff9ed46376461cf722c68
Wesley <matthew282@msn.com>
2020-04-16 21:01:15 -0700

    final version

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,71 +1,3 @@
-=begin
-
-tracked - turn counts
-
-game class
-
-
-
-
-player class
-
--name
--token
--move history
-
-
-
-
-
-game.start 
-
-1. game asks for names from both players - x
-2. game creates two new player instances - x
-3. game creates game instance with player info - x
-4. calls game turn - x
-
-game.turn
-
-1. check win - x
-2. checks turn count, assigns player.marker depending on if count is divisible by 2 - x
-3. calls get player move - x
-
-get_player_move
-
-1. create board - x
-2. ask player for move  - x
-3. check to see if valid move, assign if so - x
-
-game_finish
-
-1. prints board
-2. prints congrats
-3. ask if wants to play again
-
-
-
-
-
-
-todo:
-
-build win parameters - x
-build check_win - x
-build game_turn method - x
-build get_player_move - in progress
-build game_finish -
-
-
-
-123
-456
-789 
-
-memo: 
-
-=end
-
-
 class Game 
 	attr_accessor :board, :player1, :player2
 	def initialize(player1, player2)
@@ -77,11 +9,12 @@ class Game
 	end
 
 	def create_board
-		@board.each_index {|x|
+		@board.each_index do |x|
 			if x % 3 == 0
 				p @board[(x-2)..x] unless x == 0
 			end
-		}
+		end
+		puts "\n\n\n"
 	end
 
 	def game_turn
@@ -93,17 +26,14 @@ class Game
 	end
 
 	def self.start
-		puts "Welcome to Tic Tac Toe"
-		print "Player 1(X) please enter your name: "
+		print "Player 1 (X) please enter your name: "
 		p1 = gets.chomp
-		print "Player 2(O) please enter your name: "
+		puts "\n\n\n"
+		print "Player 2 (O) please enter your name: "
 		p2 = gets.chomp
+		puts "\n\n\n"
 		playerone = Player.new(p1, "X")
 		playertwo = Player.new(p2, "O")
-		# p1 = "wes"
-		# p2 = "bria"
-		# playerone = Player.new(p1, "X")   #temp perm players added to aid in irb testing
-		# playertwo = Player.new(p2, "O")
 		@game = Game.new(playerone, playertwo)
 		@game.game_turn
 	end
@@ -112,6 +42,7 @@ class Game
 		win = [[1,2,3], [4,5,6], [7,8,9], [1,4,7],[1,5,9], [2,5,8],[3,6,9],[3,5,7]]
 		if @turn_count >= 9
 			puts "Looks like we have a tie. What a surprise."
+			puts "\n\n\n"
 			game_finish
 		end
 
@@ -121,6 +52,7 @@ class Game
 				count += 1 if player_moves.include?(x)
 				if count == 3
 					puts "Congrats #{player.name}, you win!"
+					puts "\n\n\n"
 					create_board
 					game_finish
 				end
@@ -131,6 +63,7 @@ class Game
 	def game_finish
 		puts "Would you like to play again?"
 		ans = gets.chomp.downcase
+		puts "\n\n\n"
 		if ans == "yes"
 			Game.start
 		elsif ans == "no"
@@ -138,18 +71,18 @@ class Game
 			exit
 		else
 			puts "I'm not sure what you mean."
+			puts "\n\n\n"
 			game_finish
 		end
-	end
-
-			
+	end			
 
 	def get_move(player)
 		create_board
 		answered = 0
 		until answered == 1
-			p "Choose your move, #{player.name}, (#{player.token}): "
+			print "Choose your square, #{player.name} (#{player.token}): "
 			move = gets.chomp.to_i 		
+			puts "\n\n\n"
 				if move.to_s == @board[move]
 					@board[move] = player.token
 					answered += 1
@@ -157,11 +90,10 @@ class Game
 					game_turn
 				else
 					puts "that's not a valid move"
+					puts "\n\n\n"
 				end
 		end
 	end
-
-
 end
 
 class Player
@@ -175,17 +107,17 @@ end
 
 
 
-#helper methods
+#IRB helper methods
 
-def start      #for irb testing
+def start      #starts game
 	Game.start
 end
 
-def reload #reload current script for irb testing
+def reload #reloads script
 	load 'tic_tac_toe.rb'
 end
 
-
+puts "Welcome to Tic Tac Toe\n\n\n"
 
 Game.start
 
