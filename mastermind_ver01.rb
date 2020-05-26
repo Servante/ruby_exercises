2524d470d11d338d128401b8153dc72d75367f99
Wesley <matthew282@msn.com>
2020-05-25 18:48:35 -0700

    working on bot methods

--- mastermind_ver01.rb
+++ mastermind_ver01.rb
@@ -40,10 +40,39 @@ key pegs -  black and white (for this game we will use x's and o's)
 	-X = for correct color and position
 	-O = for correct color, but not the position
 
+methods:
+
+Game
+
+intro
+new_game
+show_board
+create_shield
+get_difficulty
+game_turn
+get_player_guess
+feedback
+play_again
+
+
+Bot 
+
+bot_guess
+consider_feedback
+
+
+
 To Do:
 
 -finish rewriting code to include new classes
-	-currently at line 131
+
+
+Create hash that will take two values(arrays) as a value
+
+h = Hash.new{|hsh,key| hsh[key] = [] }
+h['k1'].push 'a'
+h['k1'].push 'b'
+
 
 
 =end
@@ -109,64 +138,45 @@ class Game
 		@board = {}
 	end
 
-	def game_turn                              #currently here in rewrite
-		if @attempt_count > @difficulty
-			puts "\nThe code remains uncracked."
-			p @shield
-			play_again
-			exit
-		elsif @role == "codebreaker"
-			guess
+	def self.intro
+		puts "\nWelcome to Mastermind. Terminal Edition.\n"
+		puts "\nDo you wish to view the instructions?"
+		reply = gets.chomp.downcase
+		if reply == "yes"
+			puts instructions
+		elsif reply == "no"
+			puts "\n"
 		else
-			muthr_guess
+			"I do not understand."
+			intro
 		end
 	end
 
-	def guess
-		pos_no = 1
-		reply_array = []
-		puts "\n\nEnter the first letter of chosen color: \nred[r], green[g], blue[b], yellow[y], pink[p], orange[o]"
-		until pos_no >= 5
-			puts "\nWhat's your guess for position #{pos_no}?"
-			reply = gets.chomp[0].downcase
-			if @color.include?(reply)
-				reply_array << reply
-			  pos_no += 1
-			elsif reply == "x"
-				p @shield
-			else
-				puts "\nPlease use one of the key colors: r, b, o, y, p, g. Try again."
-			end
-		end
-		@board[@attempt_count] = reply_array
-		feedback(@board[@attempt_count])	
-		show_board
-	end
-
-	def play_again
-		puts "Would you like to play again?"
-		res = gets.chomp.downcase
-		if res == "yes"
-			Game.start
-		elsif res == "no"
-			puts "Thanks for playing!"
-			exit
+	def self.new_game
+		Game.intro
+		puts "What's your name?"
+		name = gets.chomp
+		puts "Would you like to be the codebreaker or codemaker?"
+		role = gets.chomp.downcase
+		diff = nil
+		if role == 'codemaker'
+			puts "Well then, let's create your secret code."
+			shield = Game.create_shield
+			diff = [8, 12].sample
+			puts "The ships computer will try and crack your code within #{diff} attempts."		
+		elsif role == 'codebreaker'
+			shield = Bot.create_shield
+			puts "MUTHR has created a code for you. If you can crack it you will surviv9, I mean win."
+			puts "\nChoose your difficulty: #{name}."
+			diff = Game.get_difficulty
 		else
-			"I don't understand."
+			puts "I am not following."
+			new_game
 		end
-	end
-
-	def self.get_difficulty
-		puts "\nInput 'easy' for 12 guess attempts and 'hard' for 8."
-		ans = gets.chomp.downcase
-		if ans == "easy"
-			return 12
-		elsif ans == "hard"
-			return 8
-		else
-			puts "\nI'm sorry, I don't understand."
-			self.get_difficulty
-		end
+		player = Player.new(name, role)
+		bot = Bot.new(role == 'codemaker' ? 'codebreaker' : 'codemaker')
+		@game = Game.new(player, bot, shield, diff)
+		@game.game_turn
 	end
 
 	def show_board
@@ -180,44 +190,18 @@ class Game
 		game_turn
 	end
 
-	def feedback(guess_arr)
-		fb_array = []
-		if guess_arr == @shield
-			puts "\nYou win! The code has been cracked open like an egg."
-			play_again
-		else
-			guess_arr.each_with_index {|v, i|
-				if @shield.include?(v) && @shield[i] == v
-					fb_array << "X"
-				elsif @shield.include?(v) && @shield[i] != v
-					fb_array << "O"
-				else
-					fb_array << "-"
-				end
-			}			
-		end
-		@board[@attempt_count] << fb_array.sort
-		@attempt_count += 1
-	end
-end
-
-class Player
-	attr_accessor :name, :role
-	def initialize(name, role)
-		@name = name
-		@role = role
-	end
-
 	def self.create_shield
 		shield = []
 		puts "\n Would you like to create the code yourself or randomly?"
 		puts "Please type 'm' for manual, or 'r' for random."
 		reply = gets.chomp[0].downcase
+		reply == nil ? "nil" : reply.downcase!
 		if reply == "m"
 			pos = 1
-			until pos >= 4
+			until pos >= 5
 				puts "Choose a color(r, b, g, y, p, o) for position #{pos}: "
-				resp = gets.chomp[0].downcase
+				resp = gets.chomp[0]
+				resp == nil ?  "nil" : resp.downcase! 
 				if ["r", "g", "b", "y", "p", "o"].include?(resp)
 					shield << resp
 					pos += 1
@@ -234,6 +218,106 @@ class Player
 		end
 		return shield
 	end
+
+	def self.get_difficulty
+		puts "\nInput 'easy' for 12 guess attempts and 'hard' for 8."
+		ans = gets.chomp.downcase
+		if ans == "easy"
+			return 12
+		elsif ans == "hard"
+			return 8
+		else
+			puts "\nI'm sorry, I don't understand."
+			self.get_difficulty
+		end
+	end
+
+	def game_turn                          
+		if @attempt_count > @difficulty
+			puts "\nThe code remains uncracked."
+			p @shield
+			play_again
+			exit
+		elsif player.role == "codebreaker"
+			guess = get_player_guess
+			@board[@attempt_count] = guess
+			feedback(guess)
+			show_board
+		else
+			feedback(bot.bot_guess)
+			show_board
+
+		end
+	end
+
+	def get_player_guess
+		pos_no = 1
+		reply_array = []
+		puts "\nAnd another one...\n" if @attempt_count > 1
+		puts "\n\nEnter the first letter of chosen color: \nred[r], green[g], blue[b], yellow[y], pink[p], orange[o]"
+		until pos_no >= 5
+			puts "\nWhat's your guess for position #{pos_no}?"
+			reply = gets.chomp[0]
+			reply == nil ?  "nil" : reply.downcase! 
+			if CODE_PEGS.include?(reply)
+				reply_array << reply
+			  pos_no += 1
+			elsif reply == "x"
+				p @shield
+			else
+				puts "\nPlease use one of the key colors: r, b, o, y, p, g. Try again."
+			end
+		end
+		puts "You guessed: #{reply_array.join(", ")}"
+		reply_array
+	end
+
+	def feedback(reply_array) #from game_turn
+		binding.pry
+		fb_array = []
+		if reply_array == @shield
+			puts "\nYou win! The code has been cracked open like an egg."
+			play_again
+		else	
+			reply_array.each_with_index {|v, i|
+				if @shield.include?(v) && @shield[i] == v     
+					fb_array << "X"
+				elsif @shield.include?(v) && @shield[i] != v
+					fb_array << "O"
+				else
+					fb_array << "-"
+				end
+			}			
+		end
+		binding.pry
+		@board[@attempt_count] = reply_array
+		@board[@attempt_count] << fb_array
+		binding.pry
+		bot.bot_review(reply_array)
+		@attempt_count += 1
+	end
+
+	def play_again
+		puts "Would you like to play again?"
+		res = gets.chomp.downcase
+		if res == "yes"
+			Game.new_game
+		elsif res == "no"
+			puts "Thanks for playing!"
+			exit
+		else
+			"I don't understand."
+		end
+	end
+end
+
+class Player
+	attr_accessor :name, :role
+	def initialize(name, role)
+		@name = name
+		@role = role
+	end
+
 end
 
 class Bot
@@ -241,76 +325,81 @@ class Bot
 	def initialize(role)
 		@name = "MUTHR"
 		@role = role
-		@memory = []
-		@knowledge = {}
+		@memory = ["r", "g", "b", "y", "p", "o"]
+		@knowledge = {"-" => Hash.new{|hsh, key| hsh[key] = []}}
 	end
 
-	def create_shield
+	def self.create_shield
 		CODE_PEGS.sample(4)
 	end
 
 	def bot_guess
 		pos_no = 1
 		reply_array = []
+		binding.pry
 		puts "The ship shudders as MUTHR reallocates processing power to your code."
 		until pos_no >= 5
-			reply_array << CODE_PEGS.sample
-			pos_no += 1
-		end
-
-		binding.pry
-
-
-		@board[@attempt_count] = reply_array
+			if @knowledge.has_key?(pos_no - 1)
+				reply_array << @knowledge[pos_no - 1]
+				pos_no += 1					
+			else 
+				reply_array << memory.sample
+				pos_no += 1
+			end
+		end		
 		puts "MUTHR's guess: #{reply_array.join(", ")}"
-		feedback(@board[@attempt_count])
-		show_board
+		return reply_array
+		binding.pry
 	end
 
-end
-
-def new_game
-	puts "What's your name?"
-	name = gets.chomp
-	puts "Would you like to be the codebreaker or codemaker?"
-	role = gets.chomp.downcase
-	diff = nil
-	if role == 'codemaker'
-		puts "Well then, let's create your secret code."
-		shield = Player.create_shield
-		diff = [8, 12].sample
-		puts "The ships computer will try and crack your code within #{diff} attempts."		
-	elsif role == 'codebreaker'
-		puts "MUTHR has created a code for you. If you can crack it you will surviv9, I mean win."
-		puts "\nChoose your difficulty, #{name}."
-		diff = Game.get_difficulty
-	else
-		puts "I am not following."
-		Game.start
-	end
-	player = Player.new(name, role)
-	bot = Bot.new(role)
-	Game.new(player, bot, shield, diff)
-end
-
-
-def intro
-	puts "\nDo you wish to view the instructions?"
-	reply = gets.chomp.downcase
-	if reply == "yes"
-		puts instructions
-	elsif reply == "no"
-		puts "\n"
-	else
-		"I do not understand."
-		intro
+	def bot_review(reply_feedback)          #clean up method
+		binding.pry
+		reply_feedback.each do |feedback|
+			feedback.each_with_index do |v, i|
+				binding.pry
+				if v == "X"
+					@knowledge[i] = v
+				elsif v == "O"
+					@memory << v 	#adds value to memory bank - needs to be reworked, ineffective
+				elsif v == "-"
+					@knowledge["-"][i].push v
+				else
+					puts "test"
+				end
+			end
+		end
 	end
 end
 
-puts "Welcome to Mastermind. Terminal Edition.\n"
 
-intro
+#knowledge["-"][1].empty?
 
-game = new_game
 
-game.game_turn
+
+Game.new_game
+
+
+
+
+
+
+
+=begin
+	
+bot_review:
+
+
+bot will take in feedback array and check if there are any x's
+	-if yes - bot will add color and index to knowledge hash
+and then it will check if there are any O's
+	-if yes - bot will add O creating colors to memory array
+
+bot_guess:
+
+if position_count equals a knowledge hash key, return the hash value
+otherwise do random sample of memory array
+
+
+
+
+=end
