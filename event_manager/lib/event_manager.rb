c8eda5cf8bd34c11cb8f1af2f39ad40b379cf012
Wesley <matthew282@msn.com>
2020-06-30 07:18:06 -0700

    final version

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,58 +1,18 @@
-=begin
-
-project-
-
-add the following features:
-
-
-Iteration: Clean Phone Numbers
-Similar to the zip codes the phone numbers suffer from multiple formats and inconsistencies. If we wanted to allow individuals to sign up for mobile alerts with the phone numbers we would need to make sure all of the numbers are valid and well-formed.
-
-If the phone number is less than 10 digits assume that it is a bad number
-If the phone number is 10 digits assume that it is good
-If the phone number is 11 digits and the first number is 1, trim the 1 and use the first 10 digits
-If the phone number is 11 digits and the first number is not 1, then it is a bad number
-If the phone number is more than 11 digits assume that it is a bad number
-
-
-
-Iteration: Time Targeting
-The boss is already thinking about the next conference: “Next year I want to make better use of our Google and Facebook advertising. Find out which hours of the day the most people registered so we can run more ads during those hours.” Interesting!
-
-Using the registration date and time we want to find out what the peak registration hours are.
-
-Ruby has a Date library which contains classes for Date and DateTime.
-
-DateTime#strptime is a method that allows us to parse date-time strings and convert them into Ruby objects.
-
-DateTime#strftime is a good reference on the characters necessary to match the specified date-time format.
-
-Use Date#hour to find out the hour of the day.
-
-
-
-
-
-Iteration: Day of the Week Targeting
-The big boss gets excited about the results from your hourly tabulations. It looks like there are some hours that are clearly more important than others. But now, tantalized, she wants to know “What days of the week did most people register?”
-
-Use Date#wday to find out the day of the week.
-
-
-	
-=end
-
-require 'pry'
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
+
+
+def restart
+	load 'event_manager.rb'
+end
 
 def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5, "0")[0..4]
 end
 
 def clean_phone(phone)
-	#binding.pry
 	phone.tr!('-''()'' ''.', '') unless phone == nil
 	if (phone.length > 10 && phone[0] != "1") || phone.length < 10
 		puts "No valid phone number provided."
@@ -61,6 +21,20 @@ def clean_phone(phone)
 	else
 		return phone
 	end
+end
+
+def reg_date_time(reg)
+	DateTime.strptime(reg, "%m/%d/%y %H:%M")
+end
+
+def feedback_report_hours(array)
+	hour_array = array.map {|row| row.strftime('%l %p')}
+	hour_array.tally.sort_by{|k, v| v}.reverse!
+end
+
+def feedback_report_day(array)
+	day_array = array.map {|row| row.strftime('%A')}
+	day_array.tally.sort_by{|k, v| v}.reverse!
 end
 
 def legislators_by_zipcode(zip)
@@ -74,6 +48,16 @@ def legislators_by_zipcode(zip)
 			roles: ['legislatorUpperBody', 'legislatorLowerBody']).officials
 	rescue
 		"You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+	end
+end
+
+def save_feedback_reports(report)
+	Dir.mkdir("feedback") unless Dir.exists?("feedback")
+
+	hours_report = "feedback/Feedback_Report.html"
+
+	File.open(hours_report, 'w') do |file|
+		file.puts report
 	end
 end
 
@@ -93,19 +77,28 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
+hours_letter = File.read "hours_layout.erb"
+erb_template_feedback = ERB.new hours_letter
+reg_date_array = []
 
 contents.each do |row|
 	id = row[0]
 	name = row[:first_name]	
 	zipcode = clean_zipcode(row[:zipcode])
 	phone = clean_phone(row[:homephone])
-	#binding.pry
 	legislators = legislators_by_zipcode(zipcode)	
-
+	reg_date_array << reg_date_time(row[:regdate])	
 	form_letter = erb_template.result(binding)
-
 	save_thank_you_letter(id, form_letter)
-
-
 end
+
+fb_hours_array = feedback_report_hours(reg_date_array)
+fb_day_array = feedback_report_day(reg_date_array)
+feedback_page = erb_template_feedback.result(binding)
+save_feedback_reports(feedback_page)	
+
+
+
+
+
 
