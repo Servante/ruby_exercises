ba6c9c7771ebb88779896e1138b5b1877c5ff93c
Wesley <matthew282@msn.com>
2020-09-29 22:53:24 -0700

    final version

--- linked_list.rb
+++ linked_list.rb
@@ -1,59 +1,6 @@
-=begin
-
-
-You will need two classes:
-
-LinkedList class, which will represent the full list.
-
-Node class, containing a #value method and a link to the #next_node, set both as nil by default.
-
--value method simply returns value
-
-
-
-Build the following methods in your linked list class:
-
-1. append(value) adds a new node containing value to the end of the list
-
-2. prepend(value) adds a new node containing value to the start of the list
-
-3. size returns the total number of nodes in the list
-
-4. head returns the first node in the list
-
-5. tail returns the last node in the list
-
-6. at(index) returns the node at the given index
-
-7. pop removes the last element from the list
-
-8. contains?(value) returns true if the passed in value is in the list and otherwise returns false.
-
-9. find(value) returns the index of the node containing value, or nil if not found.
-
-10. to_s represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
-
-
-Extra Credit
-
-01. insert_at(value, index) that inserts the node with the provided value at the given index
-
-02. remove_at(index) that removes the node at the given index. (You will need to update the links of your nodes in the list when you remove a node.)
-
-=end
-
-require "pry"
-
-def reload
-	load 'linked_lists.rb'
-end
-
-
-
 class LinkedList
 	attr_accessor :head, :tail
 	include Enumerable
-
 
 	def initialize
 		@head = nil
@@ -69,78 +16,91 @@ class LinkedList
 		end
 	end
 
+	def to_s
+		output = []
+		self.each {|n| output << "( #{n.data} ) -> " unless nil}
+		output << "nil" ; output.join("")
+	end
+
 	def append(v)
 		if @head.nil?
-			@head = v
-			@tail = v
+			@head = v ; @tail = v			
 		else
-			@tail.next = v
-			@tail = v
+			v.prev = @tail ; @tail.next = v ; @tail = v
 		end
 	end
 
 	def prepend(v)
+		binding.pry
 		if @head.nil?
-			@head = v
-			@tail = v
+			@head = v ; @tail = v
 		else
-			v.next = @head
-			@head = v
+			v.prev = nil ; v.next = @head ; @head = v		
 		end
 	end
 
 	def size 
-		#size returns the total number of nodes in the list
 		counter = 0
 		self.each {counter += 1}
 		return counter
 	end
 
 	def head
-		#returns the first node in the list
 		return @head
 	end
 
 	def tail
-		#returns the last node in the list
 		return @tail
 	end
 
 	def at(index)
-		counter = -1
-		#returns the node at the given index
-		until counter == index
-			self.each do |x|
-			binding.pry
-			
-			when counter == index
-				binding.pry
-				return x
-		end
-	end
-
+		binding.pry
+		self.each_with_index {|v, i| return v if i == index}
+		puts "There are no records at that index."
 	end
 
 	def pop
-		#removes the last element from the list
+		if @tail.nil?
+			puts "I'm sorry, there is nothing to pop."
+		else
+			binding.pry
+			@tail.prev.next = nil ; @tail = @tail.prev
+		end
 	end
 
 	def contains?(value) 
-		#returns true if the passed in value is in the list and otherwise returns false.
+		self.each {|v| return true if v.data == value}
+		return false
 	end
 
 	def find(value)
-		#returns the index of the node containing value, or nil if not found.
+		self.each_with_index {|v, i| return i if v.data == value}
+	  puts "value not found."
+	end
+
+	def insert_at(value, index)
+		counter = -1
+		self.each do |n|
+			counter += 1
+			if counter == index
+				new_node = Node.new(value, n.prev, n)
+				n.prev.next = new_node
+				n.prev = new_node
+			end
+		end
+	end
+
+	def remove_at(index)
+		counter = -1
+		self.each do |n|
+			counter += 1
+			if counter == index
+				n.prev.next = n.next
+				n.next.prev = n.prev
+			end
+		end
 	end
 end
 
-Node = Struct.new(:data, :next)
-
-
-
-
-
-
-	
-
+Node = Struct.new(:data, :prev, :next)
 
