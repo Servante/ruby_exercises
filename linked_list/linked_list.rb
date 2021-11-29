98a5577d6468ed396d585e2d6a6a228eb63fca4b
Wesley <matthew282@msn.com>
2021-11-29 13:03:23 -0700

    added linked_list and code classes

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,105 +1,48 @@
-require_relative 'node'
+=begin
+
+
+Build the following methods in your linked list class:
+
+1. #append(value) adds a new node containing value to the end of the list
+
+2. #prepend(value) adds a new node containing value to the start of the list
+
+3. #size returns the total number of nodes in the list
+
+4. #head returns the first node in the list
+
+5. #tail returns the last node in the list
+
+6. #at(index) returns the node at the given index
+
+7. #pop removes the last element from the list
+
+8. #contains?(value) returns true if the passed in value is in the list and otherwise returns false.
+
+9. #find(value) returns the index of the node containing value, or nil if not found.
+
+10. #to_s represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
+
+EC
+
+11. #insert_at(value, index) that inserts a new node with the provided value at the given index.
+
+12. #remove_at(index) that removes the node at the given index.
+
+
+=end
+
+
+require relative "node"
+include pry
 
 class LinkedList
 	attr_accessor :head, :tail
-	include Enumerable
 
-	def initialize
+	def initalize
 		@head = nil
 		@tail = nil
 	end
-
-	def each
-		return nil if @head.nil?
-		entry = @head
-		until entry.nil?
-			yield entry
-			entry = entry.next
-		end
-	end
-
-	def to_s
-		output = []
-		self.each {|n| output << "( #{n.data} ) -> " unless nil}
-		output << "nil" ; output.join("")
-	end
-
-	def append(v)
-		if @head.nil?
-			@head = v ; @tail = v			
-		else
-			v.prev = @tail ; @tail.next = v ; @tail = v
-		end
-	end
-
-	def prepend(v)
-		if @head.nil?
-			@head = v ; @tail = v
-		else
-			v.prev = nil ; v.next = @head ; @head = v		
-		end
-	end
-
-	def size 
-		counter = 0
-		self.each {counter += 1}
-		return counter
-	end
-
-	def head
-		return @head
-	end
-
-	def tail
-		return @tail
-	end
-
-	def at(index)
-		self.each_with_index {|v, i| return v if i == index}
-		puts "There are no records at that index."
-	end
-
-	def pop
-		if @tail.nil?
-			puts "I'm sorry, there is nothing to pop."
-		else
-			@tail.prev.next = nil ; @tail = @tail.prev
-		end
-	end
-
-	def contains?(value) 
-		self.each {|v| return true if v.data == value}
-		return false
-	end
-
-	def find(value)
-		self.each_with_index {|v, i| return i if v.data == value}
-	  puts "value not found."
-	end
-
-	def insert_at(value, index)
-		counter = -1
-		self.each do |n|
-			counter += 1
-			if counter == index
-				new_node = Node.new(value, n.prev, n)
-				n.prev.next = new_node
-				n.prev = new_node
-			end
-		end
-	end
-
-	def remove_at(index)
-		counter = -1
-		self.each do |n|
-			counter += 1
-			if counter == index
-				n.prev.next = n.next
-				n.next.prev = n.prev
-			end
-		end
-	end
 end
 
-Node = Struct.new(:data, :prev, :next)
 
